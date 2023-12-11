9d857e6c15dbcb76b2be1bdebcafb2f1d7d075cc
Lor92500 <149686206+Lor92500@users.noreply.github.com>
2023-12-11 19:16:33 +0100

    created pokemon list and make it dynamic

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
 const PokemonCard = () => {
+    // return (
+    //     <figure>
+    //         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+    //         <figcaption>Bulbasaur</figcaption>
+    //     </figure>
+    // );
+
+    const pokemon = pokemonList[0];
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc} alt={pokemon.name} />
+            ) : (
+                <p>???</p>
+            )}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
 }
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 export default PokemonCard;
